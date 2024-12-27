8244891b63d8731b4960ecf3c93d9811e7ded426
Antonio Eduardo Moreira <amoreira@brex.com>
2024-12-27 14:37:57 -0300

    Add UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import styles from "@/styles";
+import { useState } from "react";
 
 export default function Index() {
+  const [typedText, setTypedText] = useState("");
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>GetMyPet</Text>
+      <Text style={styles.description}>
+        O assistente que te ajuda a encontrar o Pet ideal para você
+      </Text>
+
+      <TextInput
+        style={{ ...styles.input, fontStyle: typedText ? undefined : "italic" }}
+        onChangeText={setTypedText}
+        value={typedText}
+        multiline={true}
+        numberOfLines={4}
+        placeholderTextColor="lightgray"
+        placeholder="Queria um pet terrestre, de preferência que ande sob quatro patas, não solte muito pelo e goste de ficar sozinho, pois passo a maior parte do tempo fora de casa trabalhando."
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Buscar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ styles/index.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 10,
+    fontWeight: "100",
+    fontStyle: "italic",
+  },
+  input: {
+    width: "100%",
+    marginTop: 10,
+    height: 100,
+    borderColor: "lightgray",
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 5,
+  },
+  button: {
+    marginTop: 10,
+    width: "40%",
+    textAlign: "center",
+    backgroundColor: "black",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    padding: 10,
+  },
+});
+
+export default styles;
